a0eeb81e6d7e65b9720f5b57a3aee4841cc5e621
mrzk06 <marizka7@gmail.com>
2024-06-24 08:41:57 +0700

    Initial Commit

---
+++ Leaf/LeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafApp.swift
+//  Leaf
+//
+//  Created by Marizka Ms on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
