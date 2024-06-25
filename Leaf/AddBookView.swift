1b9c79c62377165610f452baf18951d4da1ef5e4
Faiz Hadiyan Firza <faizhadiyanfirza@gmail.com>
2024-06-25 14:45:29 +0700

    feat(addBook): membuat file BookModel & AddBookView

---
+++ Leaf/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  Leaf
+//
+//  Created by Faiz Hadiyan Firza on 25/06/24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
---
+++ Leaf/BookModel.swift
@@ -0,0 +1,8 @@
+//
+//  BookModel.swift
+//  Leaf
+//
+//  Created by Faiz Hadiyan Firza on 25/06/24.
+//
+
+import SwiftUI
