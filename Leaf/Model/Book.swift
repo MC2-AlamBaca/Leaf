d9c4eabc2bd8767bc611ded930089f4fde1fb6bd
Diky Nawa Dwi Putra <96540094+DikyNawa@users.noreply.github.com>
2024-06-26 11:49:17 +0700

    feat(book) : add book model (#2)

--- Leaf/Model/Book.swift
+++ Leaf/Model/Book.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+final class Book {
+    var uuid : String = UUID().uuidString
+    var title : String
+    var author : String
+    var bookCover : Data?
+    var goal : [String]
+    
+    init(title: String, author: String, bookCover: Data? = nil, goal: [String]) {
+        self.title = title
+        self.author = author
+        self.bookCover = bookCover
+        self.goal = goal
+    }
+}
