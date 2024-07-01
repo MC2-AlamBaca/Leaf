36f67fbe6d4e593d5cc2dad8a0e92484bcd597d3
Faiz <faizhadiyanfirza@gmail.com>
2024-07-01 09:41:21 +0700

    feat(editBook): kelar.

--- Leaf/View/Book/BookListView.swift
+++ Leaf/View/Book/BookListView.swift
@@ -12,6 +12,10 @@ struct BookListView: View {
     @Environment(\.modelContext) var modelContext
     let books: [Book]
     
+    @State private var isShowingEditView = false
+    @State private var selectedBookID: UUID? // Track selected book ID for editing
+    
+    
     var body: some View {
         List {
             ForEach(books) { book in
@@ -20,24 +24,40 @@ struct BookListView: View {
                 }
                 .swipeActions(edge: .trailing, allowsFullSwipe: true) {
                     Button(role: .destructive) {
-                        deleteBook(book: book)
+                        deleteBook(book)
                     } label: {
                         Label("Delete", systemImage: "trash")
                     }
+                    Button() {
+                        // Set selectedBookID and show edit view
+                        editBook(book)
+                        selectedBookID = book.id
+                        isShowingEditView = true
+                    } label: {
+                        Label("Edit", systemImage: "pencil")
+                    }
+                    .tint(.blue)
                 }
                 .swipeActions(edge: .leading, allowsFullSwipe: true) {
                     Button {
-                        pinBook(book: book)
+                        pinBook(book)
                     } label: {
                         Label("Pin", systemImage: "pin")
                     }
                     .tint(.yellow)
                 }
             }
+            // Navigate to EditBookView with selectedBookID when isShowingEditView is true
+            .sheet(isPresented: $isShowingEditView) {
+                if let selectedBookID = selectedBookID,
+                   let selectedBook = books.first(where: { $0.id == selectedBookID }) {
+                    AddBookView(existingBook: selectedBook)
+                }
+            }
         }
     }
     
-    private func deleteBook(book: Book) {
+    private func deleteBook(_ book: Book) {
         modelContext.delete(book)
         do {
             try modelContext.save()
@@ -46,10 +66,17 @@ struct BookListView: View {
         }
     }
     
-    private func pinBook(book: Book) {
+    private func pinBook(_ book: Book) {
         // Implement pinning logic
         print("Pinning book: \(book.title)")
     }
+    
+    private func editBook(_ book: Book) {
+           // Implement your edit logic here, e.g., navigate to an edit view
+           // Example: Navigate to an edit view passing `book` as a parameter
+           // Replace with your navigation or edit action logic
+        let editView = AddBookView(existingBook: book)
+       }
 }
 //#Preview {
 //    BookListView()
