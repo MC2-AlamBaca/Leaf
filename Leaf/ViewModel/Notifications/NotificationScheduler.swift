be8b31105fbe644277cedcef1b1f0e4ebe834a75
Faiz <faizhadiyanfirza@gmail.com>
2024-07-05 08:30:55 +0700

    feat(notification): udh bisa nampilin notif dgn initial 70 seconds

--- Leaf/ViewModel/Notifications/NotificationScheduler.swift
+++ Leaf/ViewModel/Notifications/NotificationScheduler.swift
@@ -10,7 +10,7 @@ import UserNotifications
 
 struct NotificationScheduler {
     static func scheduleNotifications(for note: Note) {
-        let intervals: [TimeInterval] = [10, 86400, 604800, 1209600, 2592000, 31536000] // 1 day, 7 days, 14 days, 30 days, 365 days
+        let intervals: [TimeInterval] = [70, 86400, 604800, 1209600, 2592000, 31536000] // 70 seconds, 1 day, 7 days, 14 days, 30 days, 365 days
 
         let content = UNMutableNotificationContent()
         content.title = "Review your note"
@@ -21,6 +21,7 @@ struct NotificationScheduler {
         for interval in intervals {
             let triggerDate = note.creationDate.addingTimeInterval(interval)
             let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: triggerDate), repeats: false)
+            print(trigger)
 
             let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
             UNUserNotificationCenter.current().add(request) { error in
