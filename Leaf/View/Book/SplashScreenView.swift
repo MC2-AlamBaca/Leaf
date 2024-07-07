5b38a2adefcdbbebd769160bcd8f04653211a4a9
Diky Nawa Dwi Putra <96540094+DikyNawa@users.noreply.github.com>
2024-07-07 23:16:33 +0700

    hot-fix(splashscreen) = fix splashscreenview. file gak kebaca bro (#55)

--- Leaf/View/Book/SplashScreenView.swift
+++ Leaf/View/Book/SplashScreenView.swift
@@ -1,5 +1,5 @@
 //
-//  SplashScreen.swift
+//  SplashScreenView.swift
 //  Leaf
 //
 //  Created by Diky Nawa Dwi Putra on 07/07/24.
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct SplashScreenView: View {
     @State private var opacity = 0.0
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            Image(.logoLeaf)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-            
-            Text("Turning Pages Into Permanent Memories")
-                .font(.title2)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-        }
-        .opacity(opacity)
-        .onAppear {
-            withAnimation(.easeIn(duration: 1.0)) {
-                opacity = 1.0
+        
+        var body: some View {
+            VStack(spacing: 20) {
+                Image(.logoLeaf)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                
+                Text("Turning Pages Into Permanent Memories")
+                    .font(.title2)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+            }
+            .opacity(opacity)
+            .onAppear {
+                withAnimation(.easeIn(duration: 1.0)) {
+                    opacity = 1.0
+                }
             }
         }
-    }
 }
 
 #Preview {
