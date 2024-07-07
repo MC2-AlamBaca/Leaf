dd6c4059044776b9cb49d5ec1ad2897ad71f4193
Diky Nawa Dwi Putra <96540094+DikyNawa@users.noreply.github.com>
2024-07-07 16:01:56 +0700

    Hot fix/splashscreen (#52)
    * feat(searchNote) : add search by tiitle and content in allnoteview
    
    * hot-fix(filterNote) : fixing filternote not working before
    
    * hot-fix(pinNotes) : fixing pin notes
    
    * hot-fix(navigationTittle : fix navigation tittle on allnoteview and addnoteview
    
    * fix(addbookview, splashscreen, amd sortFilterModelView) : Fixing fontstyle, font color, and create splashscreen
    
    * hot-fix(ssplashscreenview) : fix splashscreenview

---
+++ Leaf/View/Book/SplashScreenView.swift
@@ -0,0 +1,36 @@
+//
+//  SplashScreen.swift
+//  Leaf
+//
+//  Created by Diky Nawa Dwi Putra on 07/07/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var opacity = 0.0
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(.logoLeaf)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+            
+            Text("Turning Pages Into Permanent Memories")
+                .font(.title2)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+        }
+        .opacity(opacity)
+        .onAppear {
+            withAnimation(.easeIn(duration: 1.0)) {
+                opacity = 1.0
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
